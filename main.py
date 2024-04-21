ee505b8ea216ad74a61a8d29b955552ff626149b
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-04-20 21:31:20 -0300

    adicionada mais opções e melhorias no código

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
 import vlc
 import time
+import os
+
+def song_options():
+
+    option = int(input("""
+    SONG OPTIONS:
+    
+    [1] PAUSE        
+    
+    [2] STOP
+
+    > """))
+
+    match(option):
+        case 1:
+            os.system('clear')
+            if player.is_playing:
+                player.pause()
+                option = int(input("""
+    SONG OPTIONS:
+
+    [1] CONTINUE
+
+    [2] STOP
+
+    > """))
+
+            if option == 1:
+                player.pause() # mesmo método usado pra tocar e pausar (???)
+                time.sleep(1)
+                os.system('clear')
+            elif option == 2:
+                player.stop()
+            else:
+                print("Invalid option.")
+
+        case 2:
+           player.stop()
 
 music_name = 'songs/de_nio_namnen.mp3'
 player = vlc.MediaPlayer(music_name)
@@ -10,6 +48,7 @@ player.play()
 time.sleep(1)
 
 while player.is_playing():
-    print("The music is playing.")
-
+    song_options()
+    
 print("Music finished.")
+
