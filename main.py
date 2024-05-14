64b7474773523cf8388ebb888043b6a591f3b7a0
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-05-14 16:31:55 -0300

    organização de codigo e conserto de bugs

--- main.py
+++ main.py
@@ -2,53 +2,59 @@ import vlc
 import time
 import os
 
-def song_options():
+def song_options(player):
+    while True:
+        try:
+            option = input("""
+SONG OPTIONS:
 
-    option = int(input("""
-    SONG OPTIONS:
-    
-    [1] PAUSE        
-    
-    [2] STOP
+[1] PAUSE        
+[2] STOP
 
-    > """))
+> """).strip()
 
-    match(option):
-        case 1:
-            os.system('clear')
-            if player.is_playing:
-                player.pause()
-                option = int(input("""
-    SONG OPTIONS:
+            if option == '1':
+                os.system('clear || cls')
+                if player.is_playing():
+                    player.pause()
+                    while True:
+                        option = input("""
+SONG OPTIONS:
 
-    [1] CONTINUE
+[1] CONTINUE
+[2] STOP
 
-    [2] STOP
-
-    > """))
-
-            if option == 1:
-                player.pause() # mesmo método usado pra tocar e pausar (???)
-                time.sleep(1)
-                os.system('clear')
-            elif option == 2:
+> """).strip()
+                        if option == '1':
+                            player.pause()  # Mesmo método usado para tocar e pausar
+                            time.sleep(1)
+                            os.system('clear || cls')
+                            return
+                        elif option == '2':
+                            player.stop()
+                            return
+                        else:
+                            os.system('clear || cls')
+                            print("Invalid option.")
+            elif option == '2':
                 player.stop()
+                return
             else:
+                os.system('clear || cls')
                 print("Invalid option.")
+        except ValueError:
+            os.system('clear || cls')
+            print("Invalid input.")
 
-        case 2:
-           player.stop()
-
-music_name = 'songs/de_nio_namnen.mp3'
+music_name = 'songs/Coffee.mp3'
 player = vlc.MediaPlayer(music_name)
 
 player.play()
 
-# esperando 1 segundo para dar tempo da música iniciar
+# Esperando 1 segundo para dar tempo da música iniciar
 time.sleep(1)
 
 while player.is_playing():
-    song_options()
+    song_options(player)
     
 print("Music finished.")
-
